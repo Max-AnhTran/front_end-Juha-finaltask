3ac55cd023ac67e85456e0feac1131a29f841678
Anh Tran <bhm349@myy.haaga-helia.fi>
2025-04-10 22:47:43 +0300

    fix img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className="home">
             <h1>Welcome to Fitness Center</h1>
-            <img src="./src/assets/img/home.jpg" alt="Fitness Center" />
+            <img src="/src/assets/img/home.jpg" alt="Fitness Center" />
         </div>
     )
 }
