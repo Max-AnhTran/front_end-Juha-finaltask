b8963a8c02f8cae7179ae038e7ad3889d245fc3b
Anh Tran <bhm349@myy.haaga-helia.fi>
2025-04-10 23:03:02 +0300

    img final fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className="home">
             <h1>Welcome to Fitness Center</h1>
-            <img src="./src/assets/img/home.jpg" alt="Fitness Center" />
+            <img src="https://haagahelia-my.sharepoint.com/personal/bhm349_myy_haaga-helia_fi/Documents/home.jpg" alt="Fitness Center" />
         </div>
     )
 }
